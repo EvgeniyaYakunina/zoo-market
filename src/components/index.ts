bf45f6d13182e08a203c161c8a9c9c6c3dbbce5c
EvgeniyaYakunina <Yakoonina@gmail.com>
2025-02-01 18:53:25 +0300

    fix bugs

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,6 @@
 export * from './Header'
 export * from './ImageSlider'
 export * from './Footer'
-<<<<<<< HEAD
 export * from './layout'
-=======
 export * from './ui'
 export * from './Card'
->>>>>>> dev
