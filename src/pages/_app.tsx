1f170582cd0e596b48a06a111604576cabab58af
Alex41099 <nevazhnoalikhan@gmail.com>
2025-02-06 17:49:25 +0500

    table creates card ready 80%

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/layout/layout'
+import { Provider } from 'react-redux'
+import { makeStore } from '@/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const store = makeStore()
+
   return (
-    <>
+    <Provider store={store}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </Provider>
   )
 }
