d4c234a48037789e9319daf5c15b3ec5db771426
alex <alex777kvachkov@gmail.com>
2025-02-18 13:13:48 +0300

    feature: add useErrorHandler hook
    - Update types in api.types.ts for custom types
    - Install  react-toastify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { store } from '@/app/store/store'
+import { ToastContainer } from 'react-toastify'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +11,18 @@ export default function App({ Component, pageProps }: AppProps) {
       <Provider store={store}>
         <Layout>
           <Component {...pageProps} />
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick={false}
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+          />
         </Layout>
       </Provider>
     </>
